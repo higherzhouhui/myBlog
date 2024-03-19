0b9f810ce47823e79c1205f68731674c330093c1
hc <zhouhui608@gmail.com>
2024-03-19 17:02:05 +0800

    update

--- src/app/detail/[...slug]/page.tsx
+++ src/app/detail/[...slug]/page.tsx
@@ -12,9 +12,35 @@ export default function Detail({ params }: { params: { slug: string[] } }) {
 }
 
 export async function generateStaticParams() {
-  const response = await fetch('http://localhost:3356/mydata.json')
-  const data = await response.json()
-  return data.map((item: BlogListInterface) => ({
+  // const response = await fetch('http://localhost:3356/mydata.json')
+  // const data = await response.json()
+  const staticList = [
+    {
+      id: '95379130157204',
+      title: '2024年令人眼前一亮的Web框架'
+    },
+    {
+      id: '7830128514090',
+      title: '2024 Vue 生态工具推荐'
+    },
+    {
+      id: '31229246375886',
+      title: '🔥 2024 推荐一款 Vue3 移动端模板 （⚠十个理由）'
+    },
+    {
+      id: '83644761055536',
+      title: '一个 url 就把人家网站克隆了？'
+    },
+    {
+      id: '7281921563597',
+      title: '也谈一下 30+ 程序员的出路'
+    },
+    {
+      id: '60101455073577',
+      title: '转岗，转什么？副业，红利期早已过去'
+    },
+  ]
+  return staticList.map((item: { id: string, title: string }) => ({
     slug: [String(item.id), item.title]
   }))
 }
