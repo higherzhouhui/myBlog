28e465cd9eb86835556adadc7f455f972f46e8ee
hc <zhouhui608@gmail.com>
2024-03-19 17:36:21 +0800

    UPDATE

--- src/app/detail/[...slug]/page.tsx
+++ src/app/detail/[...slug]/page.tsx
@@ -2,6 +2,42 @@
 import { BlogDetail } from "@/components/BlogDetail";
 import { BlogListInterface } from "@/interface/common";
 import { Box } from "@mui/material";
+const staticList = [
+  {
+    id: '95379130157204',
+    title: '2024年令人眼前一亮的Web框架',
+    description: '2024年令人眼前一亮的Web框架2024年令人眼前一亮的Web框架',
+  },
+  {
+    id: '7830128514090',
+    title: '2024 Vue 生态工具推荐',
+    description: 'Vue生态工具推荐',
+
+  },
+  {
+    id: '31229246375886',
+    title: '🔥 2024 推荐一款 Vue3 移动端模板 （⚠十个理由）',
+    description: '2024 推荐一款 Vue3 移动端模板 （⚠十个理由）',
+
+  },
+  {
+    id: '83644761055536',
+    title: '一个 url 就把人家网站克隆了？',
+    description: '就把人家网站克隆了？',
+
+  },
+  {
+    id: '7281921563597',
+    title: '也谈一下 30+ 程序员的出路',
+    description: '也谈一下 30+ 程序员的出路也谈一下 30+ 程序员的出路',
+
+  },
+  {
+    id: '60101455073577',
+    title: '转岗，转什么？副业，红利期早已过去',
+    description: '转岗，转什么？副业，红利期早已过去转岗，转什么？副业，红利期早已过去',
+  },
+]
 
 export default function Detail({ params }: { params: { slug: string[] } }) {
   return (
@@ -12,44 +48,19 @@ export default function Detail({ params }: { params: { slug: string[] } }) {
 }
 
 export async function generateStaticParams() {
-  const response = await fetch('https://github.com/higherzhouhui/myBlog/blob/main/public/mydata.json')
-  const data = await response.json()
-  // const staticList = [
-  //   {
-  //     id: '95379130157204',
-  //     title: '2024年令人眼前一亮的Web框架'
-  //   },
-  //   {
-  //     id: '7830128514090',
-  //     title: '2024 Vue 生态工具推荐'
-  //   },
-  //   {
-  //     id: '31229246375886',
-  //     title: '🔥 2024 推荐一款 Vue3 移动端模板 （⚠十个理由）'
-  //   },
-  //   {
-  //     id: '83644761055536',
-  //     title: '一个 url 就把人家网站克隆了？'
-  //   },
-  //   {
-  //     id: '7281921563597',
-  //     title: '也谈一下 30+ 程序员的出路'
-  //   },
-  //   {
-  //     id: '60101455073577',
-  //     title: '转岗，转什么？副业，红利期早已过去'
-  //   },
-  // ]
-  return data.map((item: { id: string, title: string }) => ({
+  // const response = await fetch('https://github.com/higherzhouhui/myBlog/blob/main/public/mydata.json')
+  // const data = await response.json()
+
+  return staticList.map((item: { id: string, title: string }) => ({
     slug: [String(item.id), item.title]
   }))
 }
 
 export async function generateMetadata({ params }: { params: { slug: string[] } }) {
-  const response = await fetch('https://github.com/higherzhouhui/myBlog/blob/main/public/mydata.json')
-  const data = await response.json()
+  // const response = await fetch('https://github.com/higherzhouhui/myBlog/blob/main/public/mydata.json')
+  // const data = await response.json()
   let metaData: BlogListInterface | any = {}
-  data.map((item: BlogListInterface) => {
+  staticList.map((item: any) => {
     if (item.id == (params.slug[0] as any)) {
       metaData = item
     }
