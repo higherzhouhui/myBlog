08ce3f2c5180a713932766831829477d4b12a8e2
hc <zhouhui608@gmail.com>
2024-07-05 02:03:44 +0800

    reset

---
+++ src/app/seekJob/page.tsx
@@ -0,0 +1,194 @@
+'use client'
+import { Avatar, Box, Button, Skeleton, Stack, Typography, styled, Badge, BadgeProps, Rating, Pagination } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { getJobListReq } from "@/service/job";
+import { JobListInterface } from "@/interface/job";
+import { calculateTimeAgo } from "@/utils/common";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { useSearchParams, useRouter } from "next/navigation";
+import { MediaQueryContext } from "@/components/BaseLayout";
+
+const BoxStyles = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.mode == 'dark' ? 'rgba(0,0,0,0.8)' : 'rgba(255,255,255,0.8)',
+  padding: 12,
+}));
+
+const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
+
+const labels: { [index: string]: string } = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+function getLabelText(value: number) {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
+
+export default function SeekJobComp() {
+  const { Sm } = useContext(MediaQueryContext);
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(searchParams.get('page') ? searchParams.get('page') as any * 1 : 1)
+  const [dataList, setDataList] = useState<JobListInterface[]>([])
+  const [backObj, setBackObj] = useState<any>({})
+  const getInitData = async (cPage: number) => {
+    setLoading(true)
+    const res = await getJobListReq({ page: cPage })
+    setLoading(false)
+    if (res.code == 200) {
+      setDataList(res.data.list)
+      setBackObj(res.data.backObj)
+    }
+  }
+
+  const handleExpand = (index: number) => {
+    const list = JSON.parse(JSON.stringify(dataList))
+    list[index].expand = !list[index].expand
+    setDataList(list)
+  }
+  const handlePage = (page: number) => {
+    setPage(page)
+    router.push(`/seekJob?page=${page}`)
+  }
+
+  useEffect(() => {
+    getInitData(page)
+  }, [page])
+
+  return <Box sx={{ padding: Sm ? '0 12px' : '12px 0' }}>
+    <BoxStyles>
+      {
+        loading ? <SkeletonList /> : <JobListItem list={dataList} total={backObj.total_count} handleExpand={handleExpand} Sm={Sm} />
+      }
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', pt: 2, borderTop: '1px solid #999' }}>
+        <Pagination onChange={(e, page) => handlePage(page)} count={backObj.total_pages} color="primary" page={page} />
+      </Box>
+    </BoxStyles>
+  </Box>
+}
+
+function JobListItem(props: { list: JobListInterface[], total: number, handleExpand: (index: number) => void, Sm: boolean }) {
+  const { list, total, handleExpand, Sm } = props
+  const handleToDetail = (id: string) => {
+    window.open(`https://eleduck.com/posts/${id}`)
+  }
+  return (
+    <Box>
+      <Stack direction="row" sx={{ mb: 2 }}>
+        <Typography color='secondary.light'>共{total}个帖子</Typography>
+      </Stack>
+      {
+        list.map((item, index) => {
+          return <Stack direction="row" key={item.id} sx={{ display: 'flex', padding: 1, alignItems: 'top', borderTop: '1px solid #999', justifyContent: 'space-between' }}>
+            <Stack direction="row" sx={{ alignItems: 'center' }} spacing={Sm ? 0 : 2}>
+              <Avatar src={item.user.avatar_url} sx={{ height: 50, width: 50, display: Sm ? 'none' : 'block' }} />
+              <Stack>
+                <StyledBadge badgeContent={item.comments_count} color="info" sx={{ width: 'fit-content' }}>
+                  <Button sx={{ pl: 0, wordBreak: 'break-all', textAlign: 'left' }} variant="text" size="large" onClick={() => handleToDetail(item.id)}>{item.title}</Button>
+                </StyledBadge>
+                <Typography security='desc' color={'GrayText'} sx={{ transition: 'all 0.3s', maxHeight: item.expand ? 300 : 0, overflow: 'hidden', wordBreak: 'break-all' }}>{item.summary}...</Typography>
+                <Stack direction="row" spacing={2} sx={{ mt: 1, alignItems: 'center', flexWrap: 'wrap', }}>
+                  <Button color={getCateColor(item.category.id)} variant="contained" size="small">
+                    {item.category.name}
+                  </Button>
+                  <Typography color='primary.light'>{item.user.nickname}</Typography>
+                  <Typography security='desc' color={'GrayText'}>发布于{calculateTimeAgo(item.published_at)}</Typography>
+                  {
+                    item.last_comment ? <>
+                      <Typography color='primary.light'>{item.last_comment?.user?.nickname}</Typography>
+                      <Typography security='desc' color={'GrayText'}>{calculateTimeAgo(item.last_comment_at)}回复</Typography>
+                    </> : null
+                  }
+                  {
+                    item.comments_count ? <Rating
+                      name="simple-controlled"
+                      readOnly
+                      value={getRatingValue(item.comments_count)}
+                    /> : null
+                  }
+                </Stack>
+              </Stack>
+            </Stack>
+            {
+              Sm ? null : <Button variant="outlined" startIcon={item.expand ? <ExpandLessIcon /> : <ExpandMoreIcon />} sx={{ height: 'fit-content', minWidth: 'fit-content', ml: 2 }} onClick={() => handleExpand(index)}>
+                {
+                  item.expand ? '收起' : '展开'
+                }
+              </Button>
+            }
+          </Stack>
+        })
+      }
+    </Box>
+  )
+}
+
+function SkeletonList() {
+  return (
+    [...Array(6)].map((item, index) => {
+      return <Skeleton sx={{ bgcolor: 'primary.light', mb: 1 }} variant="rectangular" height={150} key={index} />
+    })
+  )
+}
+
+
+function getCateColor(type: number): "inherit" | "info" | "primary" | "secondary" | "error" | "warning" | "success" {
+  let color: any = 'info'
+  if (type == 22 || type == 24 || type == 26) {
+    color = 'primary'
+  }
+  if (type == 14) {
+    color = 'secondary'
+  }
+  if (type == 15) {
+    color = 'error'
+  }
+  if (type == 10 || type == 25 || type == 19 || type == 3) {
+    color = 'warning'
+  }
+  if (type == 1 || type == 2 || type == 4) {
+    color = 'info'
+  }
+  if (type == 5) {
+    color = 'error'
+  }
+  return color
+}
+
+function getRatingValue(count: number) {
+  let value = 0
+  if (count == 1) {
+    value = 1
+  }
+  if (count > 1 && count < 5) {
+    value = 2
+  }
+  if (count >= 5 && count < 10) {
+    value = 3
+  }
+  if (count >= 10 && count < 20) {
+    value = 4
+  }
+  if (count >= 20) {
+    value = 5
+  }
+  return value
+}
---
+++ src/app/skillEdit/page.tsx
@@ -0,0 +1,220 @@
+"use client"
+import MyEditor from "@/components/MyEditor";
+import { BlogListInterface } from "@/interface/common";
+import { PostSkillListReq, getSkillListReq } from "@/service/skill";
+import { CheckCircle, Error } from "@mui/icons-material";
+import { TextField, Button, Box, Typography, SelectChangeEvent } from "@mui/material";
+import moment from "moment";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from 'react-hot-toast';
+
+export default function BlogEdit() {
+  const router = useRouter()
+  const queryId = useSearchParams().get('id')
+  const [id, setId] = useState<string | null>('')
+  const [content, setContent] = useState('')
+  const [update, setUpdate] = useState(false)
+  const [formData, setFormData] = useState<BlogListInterface>({
+    title: '',
+    subTitle: '',
+    label: [],
+    time: '',
+    uptime: '',
+    creator: '风中追风',
+    type: 'tech',
+    abstract: '',
+    logo: '',
+    content: '',
+    hrefList: [],
+  })
+  const onchangeText = (e: any, key: string) => {
+    const value = e.target.value
+    const _data = JSON.stringify(formData)
+    const n_data = JSON.parse(_data)
+    n_data[key] = value
+    setFormData(n_data)
+  }
+  const [LabelName, setLabelName] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof LabelName>) => {
+    const {
+      target: { value },
+    } = event;
+    setLabelName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+  const hanleChangeEditor = (c: string): void => {
+    setContent(c)
+  }
+  const handleConfirm = () => {
+    if (formData.title && formData.subTitle && formData.abstract && content.length && formData.logo) {
+      formData.uptime = moment().format('YYYY-MM-DD HH:mm:ss')
+      if (!id) {
+        formData.time = moment().format('YYYY-MM-DD HH:mm:ss')
+        formData.id = new Date().getTime()
+      }
+      const { content, hrefList } = handleContentRule()
+      formData.content = content
+      formData.hrefList = hrefList
+      formData.lookNum = Math.round(Math.random() * 10000)
+      PostSkillListReq(formData).then((res: any) => {
+        if (res.code == 200) {
+          toast('操作成功!', { icon: <CheckCircle />, style: { color: 'green' } })
+        } else {
+          toast(res.msg, { icon: <Error />, style: { color: 'red' } })
+        }
+      })
+    } else {
+      toast('请完善输入内容!', { icon: <Error />, style: { color: 'red' } })
+    }
+  }
+
+  const handleCancel = () => {
+    router.back()
+  }
+
+  const initData = async (id: string | null) => {
+    if (!id) {
+      return
+    }
+    try {
+      const res = await getSkillListReq({ id: id ? parseInt(id) : null })
+      if (res.code == 200) {
+        setFormData(res.data.list)
+        setContent(res.data.list.content)
+        setUpdate(true)
+      }
+    } catch {
+
+    }
+  }
+
+  const handleContentRule = () => {
+    let str = content
+    const hrefList: any[] = []
+    try {
+      const regex = /<a[^>]*>.*?<\/a>/g;
+      const matches = str.match(regex);
+      matches?.map((item: string) => {
+        const reget = /<a[^>]*>(.*?)<\/a>/g;
+
+        const matchs2: any = reget.exec(item)
+        const title = matchs2[1]
+        const reg = /\"#(.*?)\"/;
+        const matches1: any = reg.exec(item);
+        if (matches1 && matches1[1]) {
+          const name = matches1[1]
+          let itemStr = item.replace('target=""', `name="${name}"`)
+          itemStr = item.replace('target="_blank"', `name="${name}"`)
+          itemStr = item.replace('target', `name="${name}"`)
+          str = str.replace(item, itemStr)
+          hrefList.push({
+            name,
+            title,
+          })
+        }
+      })
+
+    } catch (error) {
+      console.error(error)
+    }
+
+    return { content: str, hrefList }
+  }
+
+  useEffect(() => {
+    setId(queryId)
+    initData(queryId)
+  }, [queryId])
+  return (
+    <Box sx={{ background: 'rgba(255,255,255,0.9)', height: '100%', padding: 2, overflow: 'auto' }}>
+      <Typography sx={{ pb: 2, fontSize: 22, fontWeight: 'bold' }}>
+        {
+          id ? '修改文章' : '创建新文章'
+        }
+      </Typography>
+      <Box>
+        <TextField
+          required
+          label="标题"
+          value={formData.title}
+          placeholder="请输入标题"
+          sx={{ width: '100%', mt: 2 }}
+          size="small"
+          onChange={(e) => onchangeText(e, 'title')}
+        />
+        <TextField
+          required
+          label="副标题"
+          value={formData.subTitle}
+          placeholder="请输入摘要"
+          sx={{ width: '100%', mt: 2 }}
+          size="small"
+          onChange={(e) => onchangeText(e, 'subTitle')}
+        />
+        <TextField
+          required
+          label="摘要"
+          value={formData.abstract}
+          placeholder="请输入摘要"
+          sx={{ width: '100%', mt: 2 }}
+          size="small"
+          onChange={(e) => onchangeText(e, 'abstract')}
+        />
+        <TextField
+          required
+          label="封面"
+          value={formData.logo}
+          placeholder="请输入封面图片地址"
+          sx={{ width: '100%', mt: 2 }}
+          size="small"
+          onChange={(e) => onchangeText(e, 'logo')}
+        />
+        {/* <FormControl sx={{ width: '100%', mt: 2 }} size="small" required>
+          <FormLabel id="demo-row-radio-buttons-group-label">类型</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            value={formData.type}
+            onChange={(e) => onchangeText(e, 'type')}
+          >
+            <FormControlLabel value="tech" control={<Radio />} label="技术" />
+            <FormControlLabel value="life" control={<Radio />} label="生活" />
+            <FormControlLabel value="recommend" control={<Radio />} label="推荐" />
+          </RadioGroup>
+        </FormControl> */}
+        {/* <FormControl sx={{ width: '100%', mt: 2 }} size="small" required>
+          <InputLabel id="demo-multiple-checkbox-label">标签</InputLabel>
+          <Select
+            labelId="demo-multiple-checkbox-label"
+            id="demo-multiple-checkbox"
+            multiple
+            value={LabelName || []}
+            onChange={handleChange}
+            input={<OutlinedInput label="Tag" />}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={MenuProps}
+            placeholder="请选择所属标签"
+          >
+            {names.map((name) => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={(LabelName || []).indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl> */}
+        <Typography sx={{ fontSize: 18, mt: 2, mb: 2 }}>正文</Typography>
+        <MyEditor content={content} onChangeContent={hanleChangeEditor} update={update} />
+        <Box sx={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translate(-50%, 0)' }}>
+          <Button variant="contained" color="error" onClick={() => handleCancel()} sx={{ mr: 5 }}>返回</Button>
+          <Button variant="contained" onClick={() => handleConfirm()}>保存</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
