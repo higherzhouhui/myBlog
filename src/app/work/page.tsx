03c6127cef7b620a7ae7fa86a0296e67156e7ea0
ahui <782492184@qq.com>
2024-09-26 01:09:24 +0800

    feat: update

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -40,7 +40,7 @@ export default function MediaCard() {
     { title: '上海博达数据通信有限公司官网', label: ['jquery', 'bootstrap', 'html5', 'css3', 'angular'], link: 'https://www.bdcom.com.cn/', logo: 'https://img1.baidu.com/it/u=757460992,874884504&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=326' },
     { title: '上海红星美凯龙设计云家居设计平台', label: ['REACT', 'ANT-DESIGN', 'THREE', 'TYPESCRIPT', 'CHROME-PLUGIN', 'WORKER', 'WEBGL'], link: 'https://www.mshejiyun.com/', logo: 'https://5b0988e595225.cdn.sohucs.com/images/20190407/41606f640b5744bebc67eea7bfac33c4.jpeg' },
     { title: '华夏云融航空科技官网', label: ['VUE2', 'ELEMENT-UI', 'NUST', 'SSR', 'SEO'], link: 'https://www.ceyat.net/#/', logo: 'https://gbres.dfcfw.com/Files/iimage/20240220/3FF797A80ADD30A79CB1B309043BAD94_w1269h841.png' },
-    { title: 'mac动态壁纸官网', label: ['REACT', 'NEXTJS', 'PM2', 'SSR', 'MUI', 'CANVAS'], link: 'https://www.dynamicwallpaperengine.com/', logo: 'https://www.dynamicwallpaperengine.com/_next/image?url=%2Fstatic%2Fimage%2Fzh%2F1.png&w=1920&q=75' },
+    { title: 'mac动态壁纸官网', label: ['REACT', 'NEXTJS', 'PM2', 'SSR', 'MUI', 'CANVAS'], link: 'https://www.dynamicwallpaperengine.com/', logo: '/static/images/dynamic.png' },
     { title: '重庆jizaoji信息科技有限公司官网', label: ['NUST', 'VUE3', 'ELEMENT-PLUS', 'SSR'], link: 'https://www.jizaoji.top/', logo: 'https://www.jizaoji.top/static/img/develop.5ed8710.png' },
     { title: '生物医学工程文献检索引擎', label: ['REACT', 'STYLED-COMPONENTS', 'ANT-DESIGN', 'SEO', 'SSR'], link: 'https://pubmed.ncbi.nlm.nih.gov/', logo: 'https://pbs.twimg.com/profile_banners/106895787/1607541339/600x200' },
     { title: '在线商城小程序', label: ['VUE', 'T-DESIGN', '云开发'], link: '', logo: 'https://18963215.s21i.faimallusr.com/2/ABUIABACGAAg_IDuiAYogK3d4wYwhAc4-wI.jpg' },
